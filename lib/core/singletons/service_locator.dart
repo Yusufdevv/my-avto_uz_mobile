b6dea64c61c99cc91962ba75c3d1ed01b747d359
Bahrom2101 <jabborov0121@gmail.com>
2022-12-23 17:38:59 +0500

    merge comparison conflicts resolved

--- lib/core/singletons/service_locator.dart
+++ lib/core/singletons/service_locator.dart
@@ -1,13 +1,11 @@
 import 'package:auto/core/singletons/dio_settings.dart';
-import 'package:auto/features/ad/data/datasources/ad_local_datasource.dart';
 import 'package:auto/features/ad/data/datasources/ad_remote_datasource.dart';
 import 'package:auto/features/ad/data/repositories/ad_repository_impl.dart';
 import 'package:auto/features/car_single/data/datasource/car_single_datasource.dart';
 import 'package:auto/features/car_single/data/repository/car_single_repository_impl.dart';
-import 'package:auto/features/profile/data/datasources/profile_datasource.dart';
 import 'package:auto/features/comparison/data/datasources/comparison_cars_datasource.dart';
 import 'package:auto/features/comparison/data/repositories/comparison_cars_repo_impl.dart';
-import 'package:auto/features/profile/data/datasources/profile_remote_datasource.dart';
+import 'package:auto/features/profile/data/datasources/profile_datasource.dart';
 import 'package:auto/features/profile/data/repositories/profile_repository_impl.dart';
 import 'package:get_it/get_it.dart';
 
@@ -17,8 +15,6 @@ void setupLocator() {
   serviceLocator
     ..registerLazySingleton(DioSettings.new)
     ..registerLazySingleton(
-        () => ProfileRepositoryImpl(dataSource: ProfileRemoteDataSourceImpl()))
-    ..registerLazySingleton(
         () => ComparisonDataSourceImpl(serviceLocator<DioSettings>().dio))
     ..registerLazySingleton(() => ComparisonCarsRepoImpl(
         comparisonCarsDataSource: serviceLocator<ComparisonDataSourceImpl>()))
@@ -27,8 +23,7 @@ void setupLocator() {
     ..registerLazySingleton(() => AdRepositoryImpl(
         remoteDataSource: serviceLocator<AdRemoteDataSourceImpl>()))
     ..registerLazySingleton(
-        () => CarSingleRepositoryImpl(dataSource: CarSinglenDataSourceImpl()));
-        () => ProfileRepositoryImpl(dataSource: ProfileDataSourceImpl()))
+        () => CarSingleRepositoryImpl(dataSource: CarSinglenDataSourceImpl()))
     ..registerLazySingleton(
-        () => CarSingleRepositoryImpl(dataSource: CarSinglenDataSourceImpl()));
+        () => ProfileRepositoryImpl(dataSource: ProfileDataSourceImpl()));
 }
