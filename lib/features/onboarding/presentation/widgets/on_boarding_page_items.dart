ebf74e034efe00fc3e703bbf51e8f9efd10098cb
Ravshan Xo'jamuratov <r.khujamuratov@uic.group>
2023-01-10 19:02:49 +0500

    nothing

--- lib/features/onboarding/presentation/widgets/on_boarding_page_items.dart
+++ lib/features/onboarding/presentation/widgets/on_boarding_page_items.dart
@@ -20,42 +20,44 @@ class OnBoardingItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
-      mainAxisSize: MainAxisSize.max,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 92),
-        Image.asset(
-          image,
-          width: double.maxFinite,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 32),
-          child: RichText(
-            text: TextSpan(children: [
-              WidgetSpan(child: Image.asset(icon)),
-              TextSpan(
-                text: title,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline1!
-                    .copyWith(fontSize: 36),
-              ),
-              TextSpan(
-                  text: secondText,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3!
-                      .copyWith(fontSize: 36, fontWeight: FontWeight.w700)),
-              TextSpan(
-                text: thirdText,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline1!
-                    .copyWith(fontSize: 36),
-              ),
-            ]),
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 92),
+          Image.asset(
+            image,
+            width: double.maxFinite,
           ),
-        ),
-      ],
-    );
+          Padding(
+            padding: const EdgeInsets.only(left: 32),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  WidgetSpan(child: Image.asset(icon)),
+                  TextSpan(
+                    text: title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1!
+                        .copyWith(fontSize: 36),
+                  ),
+                  TextSpan(
+                      text: secondText,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3!
+                          .copyWith(fontSize: 36, fontWeight: FontWeight.w700)),
+                  TextSpan(
+                    text: thirdText,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1!
+                        .copyWith(fontSize: 36),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
 }
