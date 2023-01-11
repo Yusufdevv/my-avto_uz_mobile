b860aeabec530010d37cfb56f7421b4e91a5ede1
Abdulaziz Malikov <79694711+abdulazizmalikov18@users.noreply.github.com>
2023-01-12 00:36:47 +0500

    fix: ads screen path

--- lib/features/ads/presentation/pages/ads_screen.dart
+++ lib/features/ads/presentation/pages/ads_screen.dart
@@ -2,7 +2,7 @@ import 'package:auto/assets/colors/color.dart';
 import 'package:auto/assets/colors/light.dart';
 import 'package:auto/assets/constants/icons.dart';
 import 'package:auto/assets/themes/theme_extensions/themed_colors.dart';
-import 'package:auto/features/ad/presentation/pages/ads/ads_body_screen.dart';
+import 'package:auto/features/ads/presentation/pages/ads_body_screen.dart';
 import 'package:auto/features/commercial/presentation/widgets/commercial_tab.dart';
 import 'package:auto/features/common/bloc/announcement_bloc/bloc/announcement_list_bloc.dart';
 import 'package:auto/features/common/bloc/get_car_model/get_car_model_bloc.dart';
