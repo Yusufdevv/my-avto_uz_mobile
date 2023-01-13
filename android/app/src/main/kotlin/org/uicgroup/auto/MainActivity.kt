74a67d64dc60085c1409d55e7ee8c511820ea26f
b.jabborov <b.jabborov@uic.group>
2023-01-13 10:51:39 +0500

    some changes with android

--- android/app/src/main/kotlin/org/uicgroup/auto/MainActivity.kt
+++ android/app/src/main/kotlin/org/uicgroup/auto/MainActivity.kt
@@ -1,11 +1,9 @@
 package org.uicgroup.auto
 
-import android.os.Bundle
 import androidx.annotation.NonNull
+import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
