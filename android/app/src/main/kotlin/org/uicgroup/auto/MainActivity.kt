2a6ddde0602ab6e820fb772f56608c5f9bb1576d
b.jabborov <b.jabborov@uic.group>
2023-01-20 18:48:36 +0500

    android reentering bug fixed

--- android/app/src/main/kotlin/org/uicgroup/auto/MainActivity.kt
+++ android/app/src/main/kotlin/org/uicgroup/auto/MainActivity.kt
@@ -3,10 +3,11 @@ package org.uicgroup.auto
 import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        MapKitFactory.setApiKey("341cdf84-6562-4231-9c00-8d00f5da4fd4") // Your generated API key
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
         super.configureFlutterEngine(flutterEngine)
     }
 }
