db0ddc636b12b8888a495ae6ac935dc607b98c8e
Shohruh_Sharipov <suvolov1106@gmail.com>
2023-01-31 17:06:57 +0500

    feat: posting_ad__permission_for_camera_and_file

--- lib/features/ad/presentation/bloc/add_photo/image_bloc.dart
+++ lib/features/ad/presentation/bloc/add_photo/image_bloc.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:bloc/bloc.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:equatable/equatable.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 part 'image_event.dart';
-
 part 'image_state.dart';
 
 class ImageBloc extends Bloc<ImageEvent, ImageState> {
@@ -18,17 +19,44 @@ class ImageBloc extends Bloc<ImageEvent, ImageState> {
           image: File(''),
         )) {
     on<PickPanaramaImageEvent>((event, emit) async {
+      Permission permissionType;
+      if (Platform.isAndroid) {
+        final androidInfo = await DeviceInfoPlugin().androidInfo;
+        if (androidInfo.version.sdkInt <= 32) {
+          permissionType = Permission.storage;
+        } else {
+          permissionType = Permission.photos;
+        }
+
+        var permission = await permissionType.status;
+        if (!permission.isGranted) {
+          permission = await permissionType.request();
+        }
+        if (!permission.isGranted) return;
+      }
+
       final image = await imagePicker.pickImage(source: ImageSource.gallery);
       print(
           '=> => => => picked panarama image path    ${image?.path}    <= <= <= <=');
       if (image != null) {
-        emit(state.copyWith(panaramaImages: [image.path, ...state.panaramaImages]));
+        emit(state
+            .copyWith(panaramaImages: [image.path, ...state.panaramaImages]));
       }
     });
     on<PickImage>((event, emit) async {
-      final image = await imagePicker.pickImage(source: event.source);
-      if (image != null) {
-        emit(state.copyWith(images: [image.path, ...state.images]));
+      var permission = await Permission.camera.status;
+      print(
+          '=> => => =>    CAMERA PERMISSION STATUS NAME: ${permission.name}     <= <= <= <=');
+      if (!permission.isGranted) {
+        permission = await Permission.camera.request();
+        print(
+            '=> => => =>     camera request status name: ${permission.name}    <= <= <= <=');
+      }
+      if (permission.isGranted) {
+        final image = await imagePicker.pickImage(source: event.source);
+        if (image != null) {
+          emit(state.copyWith(images: [image.path, ...state.images]));
+        }
       }
     });
     on<DeleteImage>((event, emit) {
