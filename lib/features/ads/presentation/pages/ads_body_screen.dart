02e9c124c60b819b5fd79b2a4413991628a39bfb
Abdulaziz Malikov <79694711+abdulazizmalikov18@users.noreply.github.com>
2023-02-02 22:55:20 +0500

    fix: ads screen nodata init

--- lib/features/ads/presentation/pages/ads_body_screen.dart
+++ lib/features/ads/presentation/pages/ads_body_screen.dart
@@ -41,76 +41,88 @@ class _AdsBodyScreenState extends State<AdsBodyScreen> {
         builder: (context, state) {
           final list =
               List<AnnouncementListEntity>.from(state.announcementList);
-          if (state.announcementList.isNotEmpty ||
-              state.status == FormzStatus.submissionInProgress) {
-            return NotificationListener<OverscrollIndicatorNotification>(
-              onNotification: (overscroll) {
-                overscroll.disallowIndicator();
-                return true;
-              },
-              child: Paginator(
-                physics: const NeverScrollableScrollPhysics(),
-                paginatorStatus: state.status,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.only(bottom: 12),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context, rootNavigator: true).push(fade(
-                          page: CarSingleScreen(
-                              id: state.announcementList[index].id)));
-                    },    behavior: HitTestBehavior.opaque,
-                    child: InfoContainer(
-                      index: index,
-                      phone: state.announcementList[index].userType == 'owner'
-                          ? state.announcementList[index].user.phoneNumber
-                          : state.announcementList[index].dealer.phoneNumber,
-                      avatarPicture: state.announcementList[index].user.avatar,
-                      carModel: state.announcementList[index].model,
-                      hasDiscount: state.announcementList[index].discount != 0,
-                      location: state.announcementList[index].region,
-                      owner: state.announcementList[index].user.name.isNotEmpty
-                          ? state.announcementList[index].user.name
-                          : state.announcementList[index].user.fullName,
-                      ownerType: state.announcementList[index].userType,
-                      publishTime: MyFunctions.getDateNamedMonthEdit(
-                          state.announcementList[index].publishedAt),
-                      subtitle: state.announcementList[index].description,
-                      year: state.announcementList[index].year,
-                      price: MyFunctions.getFormatCost(
-                          state.announcementList[index].price.toString()),
-                      discountPrice: state.announcementList[index].discount == 0
-                          ? ''
-                          : state.announcementList[index].discount.toString(),
-                      sellType: '',
-                      hasStatusInfo: state.announcementList[index].isNew,
-                      gallery: state.announcementList[index].gallery,
-                      currency: state.announcementList[index].currency,
-                      initialLike: list[index].isWishlisted,
-                      id: state.announcementList[index].id,
-                      onTapComparsion: () {},
-                      onTapFavorites: () {},
-                      initialComparsions: list[index].isComparison,
+          if (state.status.isSubmissionInProgress) {
+            return const Center(child: CupertinoActivityIndicator());
+          } else if (state.status.isSubmissionFailure) {
+            return const NoDataWidget();
+          } else if (state.status.isSubmissionSuccess) {
+            if (state.announcementList.isNotEmpty) {
+              return NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (overscroll) {
+                  overscroll.disallowIndicator();
+                  return true;
+                },
+                child: Paginator(
+                  physics: const NeverScrollableScrollPhysics(),
+                  paginatorStatus: state.status,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context, rootNavigator: true).push(fade(
+                            page: CarSingleScreen(
+                                id: state.announcementList[index].id)));
+                      },
+                      behavior: HitTestBehavior.opaque,
+                      child: InfoContainer(
+                        index: index,
+                        phone: state.announcementList[index].userType == 'owner'
+                            ? state.announcementList[index].user.phoneNumber
+                            : state.announcementList[index].dealer.phoneNumber,
+                        avatarPicture:
+                            state.announcementList[index].user.avatar,
+                        carModel: state.announcementList[index].model,
+                        hasDiscount:
+                            state.announcementList[index].discount != 0,
+                        location: state.announcementList[index].region,
+                        owner:
+                            state.announcementList[index].user.name.isNotEmpty
+                                ? state.announcementList[index].user.name
+                                : state.announcementList[index].user.fullName,
+                        ownerType: state.announcementList[index].userType,
+                        publishTime: MyFunctions.getDateNamedMonthEdit(
+                            state.announcementList[index].publishedAt),
+                        subtitle: state.announcementList[index].description,
+                        year: state.announcementList[index].year,
+                        price: MyFunctions.getFormatCost(
+                            state.announcementList[index].price.toString()),
+                        discountPrice: state.announcementList[index].discount ==
+                                0
+                            ? ''
+                            : state.announcementList[index].discount.toString(),
+                        sellType: '',
+                        hasStatusInfo: state.announcementList[index].isNew,
+                        gallery: state.announcementList[index].gallery,
+                        currency: state.announcementList[index].currency,
+                        initialLike: list[index].isWishlisted,
+                        id: state.announcementList[index].id,
+                        onTapComparsion: () {},
+                        onTapFavorites: () {},
+                        initialComparsions: list[index].isComparison,
+                      ),
                     ),
                   ),
+                  itemCount: state.announcementList.length,
+                  fetchMoreFunction: () {},
+                  hasMoreToFetch: state.announcementList.length > 9
+                      ? state.next != null
+                      : false,
+                  errorWidget: const Text('Error'),
+                  // loadingWidget: ListView.builder(
+                  //   itemBuilder: (context, index) => Padding(
+                  //     padding: const EdgeInsets.only(bottom: 12),
+                  //     child: AdsShimmerItem(),
+                  //   ),
+                  //   itemCount: 3,
+                  // ),
                 ),
-                itemCount: state.announcementList.length,
-                fetchMoreFunction: () {},
-                hasMoreToFetch: state.announcementList.length > 9
-                    ? state.next != null
-                    : false,
-                errorWidget: const Text('Error'),
-                // loadingWidget: ListView.builder(
-                //   itemBuilder: (context, index) => Padding(
-                //     padding: const EdgeInsets.only(bottom: 12),
-                //     child: AdsShimmerItem(),
-                //   ),
-                //   itemCount: 3,
-                // ),
-              ),
-            );
-          } else {
-            return const NoDataWidget();
+              );
+            } else {
+              return const NoDataWidget();
+            }
           }
+
+          return const Center(child: CupertinoActivityIndicator());
         },
       );
 }
