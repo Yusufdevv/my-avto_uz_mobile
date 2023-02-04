ae3f6b227aaad8a84d5d234f2c44b41b7ad77d9e
Yusufdevv <yusufnurullayev03@gmail.com>
2023-02-04 14:39:33 +0500

    internet blocporiver

--- lib/main.dart
+++ lib/main.dart
@@ -94,6 +94,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) => MultiBlocProvider(
         providers: [
+          BlocProvider(create: (c) => bloc),
           BlocProvider(
             create: (c) =>
                 AuthenticationBloc(AuthRepository())..add(CheckUser()),
