1e7379808a67bc561adf84d8196b3de8a82413bf
@Yusuf <y.nurullayev@uic.group>
2023-02-24 22:05:57 +0500

    delete unnecceary mainactivty

--- android/app/src/main/kotlin/org/uicgroup/auto/auto_uz_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.uicgroup.auto.auto_uz_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/org/uicgroup/avto/avto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.uicgroup.avto.uz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
