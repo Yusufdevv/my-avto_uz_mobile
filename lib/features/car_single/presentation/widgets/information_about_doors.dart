c313717655d2ca676d8520901b95c20246efa402
r.khujamuratov@uic.group <r.khujamuratov@uic.group>
2023-03-01 16:51:27 +0500

    almost nothing changed

--- lib/features/car_single/presentation/widgets/information_about_doors.dart
+++ lib/features/car_single/presentation/widgets/information_about_doors.dart
@@ -1,32 +1,32 @@
 import 'package:auto/assets/colors/color.dart';
 import 'package:auto/assets/themes/theme_extensions/themed_colors.dart';
+import 'package:auto/features/ad/presentation/pages/damage/widgets/damage_pluc_button.dart';
 import 'package:auto/features/car_single/domain/entities/damaged_parts_entity.dart';
 import 'package:auto/utils/my_functions.dart';
 import 'package:flutter/material.dart';
 
 class InformationAboutDoors extends StatelessWidget {
   const InformationAboutDoors({
-    required this.informAboutDoors,
-    required this.index,
+    required this.partName,
+    required this.damageName,
     Key? key,
   }) : super(key: key);
-
-  final List<DamagedPartsEntity> informAboutDoors;
-  final int index;
+  final String partName;
+  final String damageName;
 
   @override
   Widget build(BuildContext context) => Row(
         children: [
           Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child:
-                MyFunctions.getStatusIcon(informAboutDoors[index].damageType),
-          ),
+              padding: const EdgeInsets.only(right: 8),
+              child: DamageButton(
+                damageType: MyFunctions.stringToDamageType(damageName),
+              )),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                MyFunctions.getDamagedPartName(informAboutDoors[index].part),
+                partName,
                 style: const TextStyle(
                   color: grey,
                   fontWeight: FontWeight.w400,
@@ -34,13 +34,17 @@ class InformationAboutDoors extends StatelessWidget {
                 ),
               ),
               Text(
-                MyFunctions.getStatusTitle(informAboutDoors[index].damageType),
+                damageName.length > 22
+                    ? '${damageName.substring(0, 21).trim()}..'
+                    : damageName,
+                maxLines: 1,
                 style: TextStyle(
-                  color:
-                      Theme.of(context).extension<ThemedColors>()!.darkToWhite,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 14,
-                ),
+                    color: Theme.of(context)
+                        .extension<ThemedColors>()!
+                        .darkToWhite,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                    overflow: TextOverflow.ellipsis),
               ),
             ],
           )
