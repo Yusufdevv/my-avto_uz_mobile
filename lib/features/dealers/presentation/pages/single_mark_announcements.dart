494a5bddc14f57bc56d78adb7b8239b40fcc7b34
@Yusuf <y.nurullayev@uic.group>
2023-03-02 14:59:46 +0500

    bugfix: dealer single mar discount error fix

--- lib/features/dealers/presentation/pages/single_mark_announcements.dart
+++ lib/features/dealers/presentation/pages/single_mark_announcements.dart
@@ -1,10 +1,12 @@
 import 'package:auto/core/singletons/dio_settings.dart';
+import 'package:auto/features/car_single/presentation/car_single_screen.dart';
 import 'package:auto/features/commercial/presentation/widgets/info_container.dart';
 import 'package:auto/features/common/widgets/w_app_bar.dart';
 import 'package:auto/features/dealers/data/datasource/cars_in_marks_datasource.dart';
 import 'package:auto/features/dealers/data/repositories/cars_in_marks_repository.dart';
 import 'package:auto/features/dealers/domain/usecases/cars_in_marks_usecase.dart';
 import 'package:auto/features/dealers/presentation/blocs/cars_in_marks_bloc/cars_in_marks_bloc.dart';
+import 'package:auto/features/navigation/presentation/navigator.dart';
 import 'package:auto/features/pagination/presentation/paginator.dart';
 import 'package:auto/generated/locale_keys.g.dart';
 import 'package:auto/utils/my_functions.dart';
@@ -76,33 +78,39 @@ class _SingleMarkAnnouncementsState extends State<SingleMarkAnnouncements> {
                     final item = carsInMarksState.cars[index];
                     return Padding(
                       padding: const EdgeInsets.only(bottom: 12),
-                      child: InfoContainer(
-                        index: index,
-                        avatarPicture: item.userType == 'owner'
-                            ? item.user.avatar
-                            : item.dealer.avatar,
-                        carModel: item.absoluteCarName,
-                        hasDiscount: false,
-                        location: item.region.title,
-                        owner: item.user.fullName,
-                        ownerType: item.userType,
-                        publishTime:
-                            MyFunctions.getAutoPublishDate(item.createdAt),
-                        subtitle: item.description,
-                        year: item.year,
-                        price: double.parse(item.price),
-                        discountPrice: double.parse(item.discount),
-                        sellType: item.isRentWithPurchase
-                            ? LocaleKeys.rent_to_buy.tr()
-                            : LocaleKeys.car_sale.tr(),
-                        hasStatusInfo: item.isNew,
-                        gallery: item.gallery,
-                        currency: item.currency,
-                        initialLike: item.isWishlisted,
-                        onTapFavorites: () {},
-                        onTapComparsion: () {},
-                        initialComparsions: item.isComparison,
-                        id: item.id,
+                      child: GestureDetector(
+                        onTap: (){
+                          Navigator.of(context, rootNavigator: true)
+                              .push(fade(page: CarSingleScreen(id: item.id)));
+                        },
+                        child: InfoContainer(
+                          index: index,
+                          avatarPicture: item.userType == 'owner'
+                              ? item.user.avatar
+                              : item.dealer.avatar,
+                          carModel: item.absoluteCarName,
+                          hasDiscount: false,
+                          location: item.region.title,
+                          owner: item.user.fullName,
+                          ownerType: item.userType,
+                          publishTime:
+                              MyFunctions.getAutoPublishDate(item.createdAt),
+                          subtitle: item.description,
+                          year: item.year,
+                          price: double.parse(item.price),
+                          discountPrice: double.parse(item.discount!='' ? item.discount : '0'),
+                          sellType: item.isRentWithPurchase
+                              ? LocaleKeys.rent_to_buy.tr()
+                              : LocaleKeys.car_sale.tr(),
+                          hasStatusInfo: item.isNew,
+                          gallery: item.gallery,
+                          currency: item.currency,
+                          initialLike: item.isWishlisted,
+                          onTapFavorites: () {},
+                          onTapComparsion: () {},
+                          initialComparsions: item.isComparison,
+                          id: item.id,
+                        ),
                       ),
                     );
                   },
