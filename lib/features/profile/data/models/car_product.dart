1ca09ed3340f8377d49f2c789bcc1e14b02a39d8
r.khujamuratov@uic.group <r.khujamuratov@uic.group>
2023-03-17 09:54:11 +0500

    fix: cannot find one converter and cannot run project

--- lib/features/profile/data/models/car_product.dart
+++ lib/features/profile/data/models/car_product.dart
@@ -1,19 +1,21 @@
 import 'package:auto/features/profile/domain/entities/car_product.dart';
+import 'package:auto/features/profile/domain/entities/product_category.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'car_product.g.dart';
 
 @JsonSerializable()
 class CarProductModel extends CarProductEntity {
-  const CarProductModel(
-      {required super.category,
-      required super.name,
-      required super.image,
-      required super.price,
-      required super.count,
-      required super.carPlace,
-      required super.isAgreedPrice,
-      required super.isPopular});
+  const CarProductModel({
+    required super.category,
+    required super.name,
+    required super.image,
+    required super.price,
+    required super.count,
+    required super.carPlace,
+    required super.isAgreedPrice,
+    required super.isPopular,
+  });
 
   factory CarProductModel.fromJson(Map<String, dynamic> json) =>
       _$CarProductModelFromJson(json);
